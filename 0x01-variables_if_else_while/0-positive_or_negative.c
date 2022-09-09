922e2d1cc89d6bb15807dccb3e7f3b87ab299189
monsiisaiah1212 <monsiisaiah2525@outlook.com>
2022-09-09 06:40:27 -0700

    Initial commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,11 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n ==0)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-	else 
+	else
 	{
 		printf("%d is negative\n", n);
 	}
