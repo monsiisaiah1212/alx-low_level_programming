6805b1a7113d28c78e72f2ea8efa04c128945e79
monsiisaiah1212 <monsiisaiah2525@outlook.com>
2022-09-14 06:29:56 -0700

    Write a function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,12 +12,19 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
+
 		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+
+		return (0);
 	}
 	else
 	{
-		_putchar(48);
-		return (0);
+		_putchar('-');
+
+		return (-1);
 	}
-	_putchar('\n');
 }
