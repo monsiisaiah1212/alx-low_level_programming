89735893ef67c3cc0cb78bf2f2235219f13c9bbc
monsiisaiah1212 <monsiisaiah2525@outlook.com>
2022-09-22 15:53:51 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string
+ * to upper case.
+ * @s: input string.
+ * Return: ponter to dest.
+*/ 
+
+char *string_toupper(char *)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+		count++;
+	}
+
+	return (s);
+}
